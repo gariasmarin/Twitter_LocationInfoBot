def024f821038b41d25afa326997f09659c5f8f2
gariasmarin <58484718+gariasmarin@users.noreply.github.com>
2023-08-29 19:35:20 -0400

    Update main.py
    last update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tweepy
 import keys
+import time
+
 
 # bearer_token = keys.bearer_token
 # consumer_key = keys.api_key
@@ -20,17 +22,35 @@ def client():
         access_token=keys.access_token,
         access_token_secret=keys.access_token_secret
     )
-
     return client_
 
 
-def tweet(client_: tweepy.Client, message: str):
+def post(client_: tweepy.Client, message: str):
+    client_id = client_.get_me().data.id
     client_.create_tweet(text=message)
-
+    #client_.create_tweet(text=str(client_id))
+    response = client_.get_users_mentions(client_id)
     print('Tweeted successfully!')
+
+
+def reply(client_: tweepy.Client, message: str):
+    client_id = client_.get_me().data.id
+    response = client_.get_users_mentions(client_id)
+    client_.create_tweet(in_reply_to_tweet_id=client_id, text=message)
+    #while True:
+        #response = client_.get_users_mentions(client_id)
+
+        #if response.data != None:
+          #  for tweet in response.data:
+           #     try:
+          #          client_.create_tweet(in_reply_to_tweet_id=client_id, text=message)
+           #     except:
+            #        pass
+        #time.sleep(5)
 
 
 if __name__ == '__main__':
     client = client()
-    tweet(client, 'This was tweeted from Python')
+    post(client, 'testing78')
+    #reply(client, 'this is a reply method test ')
 # include some changes
